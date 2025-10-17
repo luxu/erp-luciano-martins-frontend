100e7d431af024df2c5ec3911e089e232c687465
Luciano Martins <luciano.martins@quay.com.br>
2025-10-17 05:25:12 -0300

    Atualizando o login

--- src/composables/UseAuthUser.js
+++ src/composables/UseAuthUser.js
@@ -1,26 +1,18 @@
 import { ref } from 'vue';
-// import useSupabase from 'boot/supabase';
 const user = ref(null);
 
 export default function useAuthUser() {
-  // const { supabase } = useSupabase();
   /**
    * Login with email and password
    */
-  // const login = async ({ email, password }) => {
-  //   const { user, error } = await supabase.auth.signIn({ email, password });
-  //   if (error) throw error;
-  //   return user;
-  // }
-
-  /**
-   * Login with google, github, etc
-   */
-  // const loginWithSocialProvider = async (provider) => {
-  //   const { user, error } = await supabase.auth.signIn({ provider });
-  //   if (error) throw error;
-  //   return user;
-  // };
+  const login = async ({ username, password }) => {
+    const { data } = await api.post('/api/v1/auth/token', {
+      username,
+      password,
+    })
+    LocalStorage.set('auth_token', data.access)
+    return data
+  }
 
   /**
    * Logout
@@ -35,63 +27,10 @@ export default function useAuthUser() {
    */
   const isLoggedIn = () => !!user.value;
 
-  /**
-   * Register
-   */
-  // const register = async ({ email, password, ...meta }) => {
-  //   const { user, error } = await supabase.auth.signUp(
-  //     { email, password },
-  //     {
-  //       // arbitrary meta data is passed as the second argument under a data key
-  //       // to the Supabase signUp method
-  //       data: meta,
-  //       // the to redirect to after the user confirms their email
-  //       // window.location wouldn't be available if we were rendering server side
-  //       // but since we're all on the client it will work fine
-  //       redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation"`
-  //     });
-  //   if (error) throw error;
-  //   return user;
-  // };
-
-  /**
-   * Update user email, password, or meta data
-   */
-  // const update = async (data) => {
-  //   const { user, error } = await supabase.auth.update(data);
-  //   if (error) throw error;
-  //   return user;
-  // };
-
-  /**
-   * Send user an email to reset their password
-   * (ie. support "Forgot Password?")
-   */
-  // const sendPasswordRestEmail = async (email) => {
-  //   const { user, error } = await supabase.auth.api.resetPasswordForEmail(email);
-  //   if (error) throw error;
-  //   return user;
-  // }
-
-  // const resetPassword = async (accessToken, newPassword) => {
-  //   const { user, error } = await supabase.auth.api.updateUser(
-  //     accessToken,
-  //     { password: newPassword },
-  //   );
-  //   if (error) throw error;
-  //   return user;
-  // };
-
   return {
     user,
-    // login,
-    // loginWithSocialProvider,
+    login,
     isLoggedIn,
     // logout,
-    // register,
-    // update,
-    // sendPasswordRestEmail,
-    // resetPassword,
-    // maybeHandleEmailConfirmation,
   };
 }
