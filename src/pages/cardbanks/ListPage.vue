deeb4ff1c2ce0e0220fe52bca0b7ada92f15d158
Luciano Martins <luciano.martins@quay.com.br>
2025-12-14 07:42:26 -0300

    fix: remove console log from handlerGetList in ListPage.vue

--- src/pages/cardbanks/ListPage.vue
+++ src/pages/cardbanks/ListPage.vue
@@ -73,7 +73,6 @@ export default defineComponent({
       try {
         loading.value = true;
         const { data } = await api.get('cardbanks');
-        console.log(data.items);        
         rows.value = data.items.map(cardbank => ({
           id: cardbank.id,
           ...cardbank,
